73e0f8ce06325ef4b586a808d33a8f98f56c9de2
mxj92 <1016706520@qq.com>
2019-12-11 21:01:12 +0800

    first get welcome page

---
+++ src/main/java/com/learn/springmall/SpringmallApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/springmall/SpringmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
